c339623e0fe10c3ab03500bd10dc74fb2b0dfe3c
Goran Knezevic <goran.knezevic@kolotree.com>
2020-03-01 00:20:54 +0100

    Home and About page

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>This app is using React and Redux to demontstrate power of each</p>
+  </div>
+);
+
+export default AboutPage;
