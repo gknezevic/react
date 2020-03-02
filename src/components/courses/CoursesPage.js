70c74e1afc933ef4e0fe45e400e6d54f91efd6b5
Goran Knezevic <goran.knezevic@kolotree.com>
2020-03-02 21:18:11 +0100

    Course form submision

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,35 @@
 import React from "react";
 
 class CoursePage extends React.Component {
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course: course });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault;
+    alert(this.state.course.title);
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h3>Coursese</h3>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          value={this.state.course.title}
+          onChange={this.handleChange}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
